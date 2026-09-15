95ee292c92d6aa2b30bc70896f02e57ea1a32414
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-09-15 20:04:37 +0300

    feat: register equipment routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import { equipmentRouter } from './routes/equipmentRoutes.js';
 
 export const app = express();
 
 app.use(express.json());
+
+app.use('/api/equipment', equipmentRouter);
 
 app.get('/api/health', (_req, res) => {
   res.status(200).json({
