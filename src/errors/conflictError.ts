95d44defce6f3aed6df383b57834af237d06a437
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-09-15 20:28:04 +0300

    refactor: extend conflict error from app error

--- src/errors/conflictError.ts
+++ src/errors/conflictError.ts
@@ -1,6 +1,8 @@
-export class ConflictError extends Error {
+import { AppError } from './appError.js';
+
+export class ConflictError extends AppError {
   constructor(message: string) {
-    super(message);
+    super('CONFLICT', message);
 
     this.name = 'ConflictError';
   }
