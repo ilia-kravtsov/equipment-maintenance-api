7148671ec09b7fe68cec1a2850ab81f90f025b9e
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-09-15 13:02:40 +0300

    feat: add Express application

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-export {};
+import express from 'express';
+
+export const app = express();
+
+app.use(express.json());
+
+app.get('/api/health', (_req, res) => {
+  res.status(200).json({
+    status: 'ok',
+  });
+});
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-export {};
+import 'dotenv/config';
+
+import { app } from './app.js';
+
+const port = Number(process.env.PORT) || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
