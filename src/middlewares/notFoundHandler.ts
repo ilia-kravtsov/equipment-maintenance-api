f81539c56f57dc2c77ef35aa70196f3597a32f99
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-09-16 17:31:27 +0300

    feat: add not found middleware

---
+++ src/middlewares/notFoundHandler.ts
@@ -0,0 +1,15 @@
+import type { RequestHandler } from 'express';
+
+import { NotFoundError } from '../errors/notFoundError.js';
+
+export const notFoundHandler: RequestHandler = (
+  req,
+  _res,
+  next,
+) => {
+  next(
+    new NotFoundError(
+      `Route ${req.method} ${req.originalUrl} not found`,
+    ),
+  );
+};
