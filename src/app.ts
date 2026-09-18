bfeaf17ef02401adc0a25ba19165d61f304039b0
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-09-18 19:27:29 +0300

    feat: enable api rate limiting

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import { requestId } from './middlewares/requestId.js';
 import { WeatherService } from './services/weatherService.js';
 import helmet from 'helmet';
 import { corsMiddleware } from './middlewares/corsMiddleware.js';
+import { apiRateLimiter } from './middlewares/rateLimitMiddleware.js';
 
 export const app = express();
 
@@ -42,6 +43,8 @@ app.use(requestId);
 app.use(helmet());
 
 app.use(corsMiddleware);
+
+app.use('/api', apiRateLimiter);
 
 app.use(
   express.json({
