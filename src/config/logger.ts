3d5d6321fb9e5debcebee449b10781afe73080b3
ilia-kravtsov <kravtsov.i.s@mail.ru>
2026-09-20 19:15:00 +0300

    fix: disable logger transport in tests

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -3,18 +3,22 @@ import pino from 'pino';
 import { config } from './index.js';
 
 export const logger =
-  config.nodeEnv === 'production'
+  config.nodeEnv === 'test'
     ? pino({
-      level: 'info',
+      level: 'silent',
     })
-    : pino({
-      level: 'debug',
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          translateTime: 'SYS:standard',
-          singleLine: false,
+    : config.nodeEnv === 'production'
+      ? pino({
+        level: 'info',
+      })
+      : pino({
+        level: 'debug',
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+            singleLine: false,
+          },
         },
-      },
-    });
+      });
